5df202a26c0170d49ab827137ed7b9eb1e4997ef
Archit26k <76521139+Archit26k@users.noreply.github.com>
2021-01-14 18:50:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var counter = 0;
+var timeleft = 65;
+
+function convertSeconds(s)
+{
+  
+  var min = floor(s/60);
+  var sec = s % 60
+  return nf(min,2) + ':'+ nf(sec,2) ;
+}
+
+function setup() {
+  noCanvas();
+  
+  
+var params = getURLParams();
+ 
+ if(params.minute) {
+   var min = params.minute;
+timeleft = min * 60;  
+ }
+  
+
+  
+  var timer = select('#timer');
+  timer.html(convertSeconds(timeleft - counter))
+   
+  function timeIt()
+  {
+    counter++;
+   timer.html(convertSeconds(timeleft - counter))
+  } 
+  
+   
+  setInterval( timeIt , 1000)
+}
+
+// function draw() {
+//   background(220);
+// }
